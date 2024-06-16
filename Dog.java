783abe3f8a87f5533280b52c7961fc12b63c4503
dhfkdlsj <105478203+dhfkdlsj@users.noreply.github.com>
2024-06-16 17:45:47 +0900

    생성자 오버로딩

---
+++ Dog.java
@@ -0,0 +1,16 @@
+public class Dog{
+    String name;
+    int age;
+    Dog() {
+        this.name ="강아지";
+        this.age = 1;
+    }
+    Dog(String name) {
+        this.name = name;
+        this.age = 3;
+    }
+    Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+} //Dog 생성자 오버로딩
---
+++ DogTest.java
@@ -0,0 +1,8 @@
+public class DogTest {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        System.out.println("dog.name : "+dog.name);
+        Dog dog1 = new Dog("바다");
+        System.out.println("dog1.name : "+dog1.name);
+    }
+} //Dog 객체 생성후 출력
